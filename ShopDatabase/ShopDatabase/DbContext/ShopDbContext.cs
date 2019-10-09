7cc98b1c1563457463e57d8b4d261f24f2699ca3
Heino <heinoolen@gmail.com>
2019-10-09 13:23:19 +0300

    codedb

---
+++ ShopDatabase/ShopDatabase/DbContext/ShopDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ShopDatabase.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopDatabase
+{
+    class ShopDbContext : DbContext
+    {
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<Food> Foods { get; set; }
+    }
+}
